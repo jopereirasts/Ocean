fa9a13eb589dc26da79447e78cb89867519dba8c
[jo] <jonespjunior@gmail.com>
2023-11-09 21:18:54 -0400

    pointer

--- ocean/src/App.jsx
+++ ocean/src/App.jsx
@@ -4,7 +4,10 @@ function App() {
   return (
     <>
       <div className='card'>
-        <h2> Rick Sanchez</h2>
+        <div className='title'>
+          <h2> Rick Sanchez</h2>
+        </div>
+        
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
       </div>
     </>
