4a462966b1e82eb63bba1450f9ef60387809449d
[jo] <jonespjunior@gmail.com>
2023-11-13 19:49:12 -0400

    arrumei as tags

--- ocean/src/App.jsx
+++ ocean/src/App.jsx
@@ -4,16 +4,16 @@ function App() {
   return (
     <>
       <div className='card'>
-        <h2 className='title'> Rick Sanchez</h2>
+        <h2> Rick Sanchez</h2>
         <div className='tags'>
           <div className='tag'>
-            <p>Humano</p>
+            Status: Vivo
           </div>
           <div className='tag'>
-            <p>Pai</p>
+            Espécie: Humana
           </div>
           <div className='tag'>
-            <p>Cientista</p>
+            Origem: Terra C-137
           </div>
         </div>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
